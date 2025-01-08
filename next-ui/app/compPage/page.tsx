23dadc1e3335d4051e9a90f8072ab294d36166f4
khaymanii <kennethakpo61@gmail.com>
2025-01-08 17:56:44 +0100

    refactor: rename compPage to comp and update layout; remove compDetailsPage and add new details page; enhance tailwind config with brown color palette

--- next-ui/app/compPage/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function page() {
-  return <section>compPage</section>;
-}
-
-export default page;
--- next-ui/tailwind.config.ts
+++ next-ui/tailwind.config.ts
@@ -10,8 +10,18 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        brown: {
+          50: "#fdf8f6",
+          100: "#f3eae4",
+          200: "#e4cdc0",
+          300: "#d3ad95",
+          400: "#ba8463",
+          500: "#9c6849", // Primary brown shade
+          600: "#80543a",
+          700: "#634029",
+          800: "#422a19",
+          900: "#24150b",
+        },
       },
     },
   },
