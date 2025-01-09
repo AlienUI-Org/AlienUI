ab9c3322eb7141791ad61b8a001d9e1eb35e54c8
khaymanii <kennethakpo61@gmail.com>
2025-01-09 09:29:01 +0100

    feat: integrate Banner component into layout; enhance Inspiration section with images and styling

--- next-ui/app/page.tsx
+++ next-ui/app/page.tsx
@@ -1,4 +1,3 @@
-import Banner from "@/components/Banner";
 import Hero from "@/components/Hero";
 import WhyUseAlienUI from "@/components/WhyUseAlienUI";
 import NewsLetter from "@/components/NewsLetter";
@@ -8,11 +7,10 @@ import Inspiration from "@/components/Inspiration";
 export default function Home() {
   return (
     <section>
-      <Banner />
       <Hero />
       <WhyUseAlienUI />
-      <NewsLetter />
       <Inspiration />
+      <NewsLetter />
       <FaQ />
     </section>
   );
--- next-ui/components/Inspiration.tsx
+++ next-ui/components/Inspiration.tsx
@@ -1,7 +1,36 @@
 import React from "react";
+import Image from "next/image";
 
 function Inspiration() {
-  return <section>Inspiration</section>;
+  return (
+    <section className="mb-16">
+      {" "}
+      <div className="max-w-7xl mx-auto px-6 text-center">
+        <h1 className="text-3xl font-bold text-brown-800 sm:text-4xl text-center mb-2">
+          Inspiration
+        </h1>
+        <p className="mt-4 text-lg text-gray-800 mb-8 text-center mx-4">
+          This project wouldn&apos;t have been possible without the great work
+          by community members and inspiration from these libraries.
+        </p>
+
+        <div className="mt-10 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+          <div className="flex flex-col items-center bg-brown-600 rounded-md p-6 shadow-lg hover:shadow-xl hover:scale-105 transition-transform duration-300 ease-in-ou">
+            <Image src="/images/native.png" alt="" width={300} height={300} />
+          </div>
+
+          <div className="flex flex-col items-center border border-brown-600 rounded-md p-6 shadow-lg hover:shadow-xl hover:scale-105 transition-transform duration-300 ease-in-ou">
+            {" "}
+            <Image src="/images/hyperui.webp" alt="" width={300} height={300} />
+          </div>
+
+          <div className="flex flex-col items-center bg-brown-600 rounded-md p-6 shadow-lg hover:shadow-xl hover:scale-105 transition-transform duration-300 ease-in-ou">
+            <Image src="/images/shadcnui.png" alt="" width={300} height={300} />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Inspiration;
