ef004ba89cf4662f0604e802c0d553954c6dee52
Ken <116851212+khaymanii@users.noreply.github.com>
2025-01-18 00:03:48 +0100

    Update GalaxyCard.tsx

--- next-ui/components/comp/Card/GalaxyCard.tsx
+++ next-ui/components/comp/Card/GalaxyCard.tsx
@@ -4,7 +4,7 @@ import { FaChartBar } from "react-icons/fa";
 function GalaxyCard() {
   return (
     <div className="flex justify-center">
-      <div className="w-3/5 h-44 rounded-2xl bg-[#fdf8f6] p-6">
+      <div className="w-3/5 h-52 rounded-2xl bg-[#fdf8f6] p-6">
         <div className="flex items-center justify-center w-12 h-12 bg-white text-[#24150b] rounded-full mb-6">
           <FaChartBar className="text-[#ba8463]" size={24} />
         </div>
