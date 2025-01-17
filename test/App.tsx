3e9765743fb1e3e741ac41b5dbbb1bb55686a7a8
khaymanii <kennethakpo61@gmail.com>
2025-01-17 16:19:45 +0100

    feat: add GalaxyTab component and integrate it into App layout

--- test/App.tsx
+++ test/App.tsx
@@ -1,10 +1,12 @@
 import { StyleSheet, ScrollView } from "react-native";
 import GalaxyStep from "./components/Step/GalaxyStep";
+import GalaxyTab from "./components/Tab/GalaxyTab";
 
 export default function App() {
   return (
     <ScrollView style={styles.container}>
       <GalaxyStep />
+      <GalaxyTab />
     </ScrollView>
   );
 }
---
+++ test/components/Tab/GalaxyTab.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const GalaxyTab = () => {
+  return (
+    <View>
+      <Text>GalaxyTab</Text>
+    </View>
+  );
+};
+
+export default GalaxyTab;
+
+const styles = StyleSheet.create({});
