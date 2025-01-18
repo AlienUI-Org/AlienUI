31524db2bb7dc31f8519f95f34b15f0ecfc38830
Ken <116851212+khaymanii@users.noreply.github.com>
2025-01-18 11:11:33 +0100

    Update page.tsx

--- next-ui/app/comp/page.tsx
+++ next-ui/app/comp/page.tsx
@@ -24,7 +24,7 @@ function page() {
               If you plan to use <strong>NativeWind</strong> for styling, ensure
               your app is properly configured. Check the{" "}
               <a
-                href="https://nativewind.dev/docs/installation"
+                href="https://nativewind.dev"
                 className="text-blue-600 underline"
               >
                 NativeWind configuration guide
