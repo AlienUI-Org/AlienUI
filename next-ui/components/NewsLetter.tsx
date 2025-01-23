2105ec2d0b8b41788f8646eb914e7d353e8e2b60
khaymanii <kennethakpo61@gmail.com>
2025-01-23 16:03:46 +0100

    Add Firebase integration for newsletter and suggestion forms; update .gitignore and package.json

--- next-ui/components/NewsLetter.tsx
+++ next-ui/components/NewsLetter.tsx
@@ -1,10 +1,35 @@
+"use client";
 import React from "react";
+import { useState } from "react";
+import { addDoc, collection } from "firebase/firestore";
+import { db } from "@/firebaseConfig/firebase";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function NewsLetter() {
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    try {
+      await addDoc(collection(db, "newsletter"), {
+        email,
+        createdAt: new Date(),
+      });
+
+      toast.success("Newsletter subscribed successfully!");
+
+      setEmail("");
+    } catch (err) {
+      console.error("Error subscribing :", err);
+      toast.error("Failed to subscribe. Please try again.");
+    }
+  };
+
   return (
     <section className="py-20 text-black bg-brown-100">
       <div className="max-w-4xl mx-auto px-6 text-center">
-        {/* Heading */}
         <h1 className="text-3xl font-bold text-brown-800 sm:text-4xl text-center mb-2">
           Stay Updated with AlienUI
         </h1>
@@ -13,13 +38,17 @@ function NewsLetter() {
           components and community news delivered straight to your inbox.
         </p>
 
-        {/* Form */}
-        <form className="mt-8 flex flex-col sm:flex-row gap-4 justify-center">
+        <form
+          className="mt-8 flex flex-col sm:flex-row gap-4 justify-center"
+          onSubmit={handleSubmit}
+        >
           <input
             type="email"
             placeholder="Enter your email address"
-            className="w-full sm:w-2/3 px-4 py-3 rounded-md text-gray-800 border border-gray-300 focus:outline-none focus:ring-1 focus:ring-brown-700"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
             required
+            className="w-full sm:w-2/3 px-4 py-3 rounded-md text-gray-800 border border-gray-300 focus:outline-none focus:ring-1 focus:ring-brown-700"
           />
           <button
             type="submit"
@@ -29,6 +58,7 @@ function NewsLetter() {
           </button>
         </form>
       </div>
+      <ToastContainer position="top-right" autoClose={3000} hideProgressBar />
     </section>
   );
 }
--- next-ui/components/Suggestion.tsx
+++ next-ui/components/Suggestion.tsx
@@ -1,5 +1,9 @@
 "use client";
 import React, { useState } from "react";
+import { addDoc, collection } from "firebase/firestore";
+import { db } from "@/firebaseConfig/firebase";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function Suggestion() {
   const [formData, setFormData] = useState({
@@ -8,8 +12,6 @@ function Suggestion() {
     message: "",
   });
 
-  const [submitted, setSubmitted] = useState(false);
-
   const handleChange = (
     e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
   ) => {
@@ -17,11 +19,28 @@ function Suggestion() {
     setFormData((prev) => ({ ...prev, [name]: value }));
   };
 
-  const handleSubmit = (e: React.FormEvent) => {
+  const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-    console.log("Feedback Submitted:", formData);
-    setSubmitted(true);
-    setFormData({ name: "", email: "", message: "" }); // Reset the form
+
+    const { name, email, message } = formData;
+
+    try {
+      await addDoc(collection(db, "suggestion"), {
+        name,
+        email,
+        message,
+        createdAt: new Date(),
+      });
+
+      toast.success(
+        "Suggestion sent successfully, Thank you for your feedback! We appreciate your input."
+      );
+
+      setFormData({ name: "", email: "", message: "" });
+    } catch (err) {
+      console.error("Error submitting suggestion:", err);
+      toast.error("Failed to send the message. Please try again.");
+    }
   };
 
   return (
@@ -30,82 +49,74 @@ function Suggestion() {
         <h2 className="text-3xl sm:text-4xl font-bold text-brown-800 mb-4 text-center">
           Suggestion & Feedback Box
         </h2>
-        {submitted ? (
-          <div className="text-green-600 font-medium">
-            Thank you for your feedback! We appreciate your input.
-          </div>
-        ) : (
-          <form onSubmit={handleSubmit} className="space-y-4">
-            {/* Name Field */}
-            <div>
-              <label
-                htmlFor="name"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Name
-              </label>
-              <input
-                type="text"
-                id="name"
-                name="name"
-                value={formData.name}
-                onChange={handleChange}
-                className="mt-1 block w-full px-4 py-2 border border-brown-300 rounded-md focus:ring-brown-500 focus:border-brown-500"
-                placeholder="Your Name"
-                required
-              />
-            </div>
 
-            {/* Email Field */}
-            <div>
-              <label
-                htmlFor="email"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Email
-              </label>
-              <input
-                type="email"
-                id="email"
-                name="email"
-                value={formData.email}
-                onChange={handleChange}
-                className="mt-1 block w-full px-4 py-2 border border-brown-300 rounded-md  focus:ring-brown-500 focus:border-brown-500"
-                placeholder="Your Email"
-                required
-              />
-            </div>
-
-            {/* Message Field */}
-            <div>
-              <label
-                htmlFor="message"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Message
-              </label>
-              <textarea
-                id="message"
-                name="message"
-                value={formData.message}
-                onChange={handleChange}
-                rows={4}
-                className="mt-1 block w-full px-4 py-2 border border-brown-300 rounded-md focus:ring-brown-500 focus:border-brown-500"
-                placeholder="Your suggestions or feedback"
-                required
-              />
-            </div>
-
-            {/* Submit Button */}
-            <button
-              type="submit"
-              className="w-full bg-brown-600 text-white py-2 px-4 rounded-md hover:bg-brown-800 focus:outline-none focus:ring-2 focus:ring-brown-500"
+        <form onSubmit={handleSubmit} className="space-y-4">
+          <div>
+            <label
+              htmlFor="name"
+              className="block text-sm font-medium text-gray-700"
             >
-              Submit
-            </button>
-          </form>
-        )}
+              Name
+            </label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+              className="mt-1 block w-full px-4 py-2 border border-brown-300 rounded-md focus:ring-brown-500 focus:border-brown-500"
+              placeholder="Your Name"
+              required
+            />
+          </div>
+
+          <div>
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              className="mt-1 block w-full px-4 py-2 border border-brown-300 rounded-md  focus:ring-brown-500 focus:border-brown-500"
+              placeholder="Your Email"
+              required
+            />
+          </div>
+
+          <div>
+            <label
+              htmlFor="message"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Message
+            </label>
+            <textarea
+              id="message"
+              name="message"
+              value={formData.message}
+              onChange={handleChange}
+              rows={4}
+              className="mt-1 block w-full px-4 py-2 border border-brown-300 rounded-md focus:ring-brown-500 focus:border-brown-500"
+              placeholder="Your suggestions or feedback"
+              required
+            />
+          </div>
+
+          <button
+            type="submit"
+            className="w-full bg-brown-600 text-white py-2 px-4 rounded-md hover:bg-brown-800 focus:outline-none focus:ring-2 focus:ring-brown-500"
+          >
+            Submit
+          </button>
+        </form>
       </div>
+      <ToastContainer position="top-right" autoClose={3000} hideProgressBar />
     </section>
   );
 }
