34a63e0f3d37613e5a61aa5784c629929cd0da2b
Ken <116851212+khaymanii@users.noreply.github.com>
2025-01-23 20:39:48 +0100

    Update EarthTab.tsx

--- next-ui/components/comp/Tab/EarthTab.tsx
+++ next-ui/components/comp/Tab/EarthTab.tsx
@@ -4,19 +4,19 @@ const EarthTab = () => {
   return (
     <div className="flex w-[400px] items-center justify-around rounded bg-black py-4">
       <div>
-        <p className="text-white font-bold">Home</p>
+        <p className="text-white">Home</p>
       </div>
       <div>
-        <p className="text-white font-bold">Contact</p>{" "}
+        <p className="text-white">Contact</p>{" "}
       </div>
       <div>
-        <p className="text-white font-bold">Cart</p>
+        <p className="text-white">Cart</p>
       </div>
       <div>
-        <p className="text-white font-bold">Charts</p>
+        <p className="text-white">Charts</p>
       </div>
       <div>
-        <p className="text-white font-bold">Settings</p>
+        <p className="text-white">Settings</p>
       </div>
     </div>
   );
