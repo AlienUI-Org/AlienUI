63da361160db4c4f05081f9801b58535411a5e8e
Ken <116851212+khaymanii@users.noreply.github.com>
2025-01-29 19:55:36 +0100

    Update PlutoCard.tsx

--- next-ui/components/comp/Card/PlutoCard.tsx
+++ next-ui/components/comp/Card/PlutoCard.tsx
@@ -4,8 +4,8 @@ import { FaChartBar } from "react-icons/fa";
 function PlutoCard() {
   return (
     <div className="flex justify-center">
-      <div className="w-3/5 h-52 rounded-2xl bg-black p-6">
-        <div className="flex items-center justify-center w-12 h-12 bg-white text-[#24150b] rounded-full mb-6">
+      <div className="w-3/5 h-52 bg-black p-6">
+        <div className="flex items-center justify-center w-12 h-12 bg-white rounded-full mb-6">
           <FaChartBar className="text-black" size={24} />
         </div>
         <p className="text-lg text-white font-bold">Bar chart showing energy usage</p>
