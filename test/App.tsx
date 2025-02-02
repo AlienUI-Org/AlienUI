ab83bc9beda3d0708d242ab5a99059b478b4b52a
khaymanii <kennethakpo61@gmail.com>
2025-02-02 23:38:15 +0100

    refactor: remove unused Toast components from App and simplify render logic

--- test/App.tsx
+++ test/App.tsx
@@ -1,27 +1,9 @@
 import React from "react";
 import "./global.css";
 import { StyleSheet, ScrollView } from "react-native";
-import GalaxyTable from "./components/Table/GalaxyTable";
-import GalaxyToast from "./components/Toast/GalaxyToast";
-import EarthToast from "./components/Toast/EarthToast";
 
 export default function App() {
-  return (
-    <ScrollView style={styles.container}>
-      <EarthToast />
-      <EarthToast />
-      <EarthToast />
-      <EarthToast />
-      <EarthToast />
-      <EarthToast />
-      <EarthToast />
-      <EarthToast />
-      <EarthToast />
-
-      <GalaxyToast />
-      <EarthToast />
-    </ScrollView>
-  );
+  return <ScrollView style={styles.container}></ScrollView>;
 }
 
 const styles = StyleSheet.create({
