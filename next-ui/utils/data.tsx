0396f9ef96e7c7142166331781af15c03eaa4cbc
Ken <116851212+khaymanii@users.noreply.github.com>
2025-02-03 01:29:10 +0100

    Update data.tsx

--- next-ui/utils/data.tsx
+++ next-ui/utils/data.tsx
@@ -69,7 +69,7 @@ const Accordion = () => {
     },
   ];
 
-  const toggleAccordion = (index: any) => {
+  const toggleAccordion = (index) => {
     setOpenIndex(index === openIndex ? null : index);
   };
 
