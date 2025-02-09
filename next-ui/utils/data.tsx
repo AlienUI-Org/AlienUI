f63aac92379579d0a73a20d575b33fd16bf1cede
khaymanii <kennethakpo61@gmail.com>
2025-02-09 21:45:09 +0100

    edit pluto card

--- next-ui/utils/data.tsx
+++ next-ui/utils/data.tsx
@@ -509,13 +509,11 @@ import AntDesign from "@expo/vector-icons/AntDesign"
 
 const Card = () => {
   return (
-    <View className="flex justify-center">
       <View className="w-3/5 h-44 bg-black p-6">
         <View className="flex items-center justify-center w-12 h-12 bg-white rounded-full mb-6">
           <AntDesign name="barschart" size={24} color="#000000" />
         </View>
         <Text className="text-lg text-white font-bold">Bar chart showing energy usage</Text>
-      </View>
     </View>
   );
 };
