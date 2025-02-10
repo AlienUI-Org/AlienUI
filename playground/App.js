b1dc070bbb747fc8d488abb32ce1803f499f77a6
khaymanii <kennethakpo61@gmail.com>
2025-02-10 10:53:38 +0100

    refactor: remove TempScreen1 component from App for cleaner structure

--- playground/App.js
+++ playground/App.js
@@ -3,11 +3,7 @@ import "./global.css";
 import TempScreen1 from "./screens/TempScreen1";
 
 export default function App() {
-  return (
-    <ScrollView className="bg-blue-50" style={styles.container}>
-      <TempScreen1 />
-    </ScrollView>
-  );
+  return <ScrollView style={styles.container}></ScrollView>;
 }
 
 const styles = StyleSheet.create({
