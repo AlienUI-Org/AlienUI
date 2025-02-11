fcec0a1ad84832a6e8e574fd0f1fcfcdbfb4144d
khaymanii <kennethakpo61@gmail.com>
2025-02-11 10:31:54 +0100

    added padding

--- playground/components/comp1/Pocket.jsx
+++ playground/components/comp1/Pocket.jsx
@@ -9,24 +9,24 @@ const Pocket = () => {
         <Text className="text-blue-700 font-semibold text-xs">See all</Text>
       </View>
       <View className="ml-4 flex-row gap-2.5">
-        <Pressable className="text-white text-xs bg-[#1237FC] w-12 px-2 py-1 rounded-full items-center justify-center">
+        <Pressable className="text-white text-xs bg-[#1237FC] w-12 px-2 py-2 rounded-full items-center justify-center">
           All
         </Pressable>
-        <Pressable className="text-black text-xs bg-[#CED3F0] w-auto px-4 rounded-full items-center justify-center">
+        <Pressable className="text-black text-xs bg-[#CED3F0] w-auto px-4 py-2 rounded-full items-center justify-center">
           Investment
-        </Pressable>{" "}
-        <Pressable className="text-black text-xs bg-[#CED3F0] w-auto px-4 rounded-full items-center justify-center">
+        </Pressable>
+        <Pressable className="text-black text-xs bg-[#CED3F0] w-auto px-4 py-2 rounded-full items-center justify-center">
           Savings
-        </Pressable>{" "}
-        <Pressable className="text-black text-xs bg-[#CED3F0] w-auto px-4 rounded-3xl items-center justify-center">
+        </Pressable>
+        <Pressable className="text-black text-xs bg-[#CED3F0] w-auto px-4 py-2 rounded-3xl items-center justify-center">
           Targets
-        </Pressable>{" "}
-        <Pressable className="text-black text-xs bg-[#CED3F0] w-12 px-3 rounded-2xl items-center justify-center">
+        </Pressable>
+        <Pressable className="text-black text-xs bg-[#CED3F0] w-12 px-3 py-2  rounded-2xl items-center justify-center">
           Bills
-        </Pressable>{" "}
-        <Pressable className="text-black text-xs bg-[#CED3F0] w-auto px-3 rounded-2xl items-center justify-center">
+        </Pressable>
+        <Pressable className="text-black text-xs bg-[#CED3F0] w-auto px-3 py-2 rounded-2xl items-center justify-center">
           Food
-        </Pressable>{" "}
+        </Pressable>
       </View>
     </View>
   );
