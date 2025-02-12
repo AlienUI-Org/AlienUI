6c78d373b312347b96edb2731b72f46e59ab7d7d
khaymanii <kennethakpo61@gmail.com>
2025-02-12 12:52:22 +0100

    Remove TempScreen2 component from App.js

--- playground/App.js
+++ playground/App.js
@@ -1,12 +1,9 @@
 import { StyleSheet, ScrollView } from "react-native";
 import "./global.css";
-import TempScreen2 from "./screens/TempScreen2";
 
 export default function App() {
   return (
-    <ScrollView style={styles.container} className="font-sans">
-      <TempScreen2 />
-    </ScrollView>
+    <ScrollView style={styles.container} className="font-sans"></ScrollView>
   );
 }
 
