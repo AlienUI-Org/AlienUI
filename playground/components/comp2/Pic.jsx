8fe9503c90ea4a25e1e47cdd52c9967f179c507b
khaymanii <kennethakpo61@gmail.com>
2025-02-12 12:44:46 +0100

    Update Pic component with new image and revised text content

--- playground/components/comp2/Pic.jsx
+++ playground/components/comp2/Pic.jsx
@@ -3,9 +3,9 @@ import React from "react";
 
 const Pic = () => {
   return (
-    <View className="items-center justify-center mt-8">
+    <View className="items-center justify-center mt-6">
       <Image
-        source={require("../../assets/images/fez12.svg")}
+        source={require("../../assets/images/fez13.svg")}
         className="rounded-3xl mb-4"
         resizeMode="cover"
       />
@@ -15,10 +15,10 @@ const Pic = () => {
         <View className="w-3 h-3 rounded-full bg-[#3F3F3F]"></View>
       </View>
       <View className="items-center justify-center mb-6">
-        <Text className="text-4xl font-semibold mb-2">Affordable pricing</Text>
-        <Text className="text-center text-xl mx-3">
-          Enjoy competitive rates for both local and international deliveries,
-          making shipping cost-effective.
+        <Text className="text-4xl font-semibold mb-2">Global Reach</Text>
+        <Text className="text-center text-xl px-3.5">
+          Access delivery services in over 200 cities worldwide, ensuring broad
+          coverage for your shipping needs.
         </Text>
       </View>
       <Pressable className="bg-transparent border border-[#45C734] py-4 rounded-full flex items-center justify-center w-[90%] mb-3">
