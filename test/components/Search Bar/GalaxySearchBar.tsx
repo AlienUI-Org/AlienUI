8608cabe35a59aa9f681acfe724b8ff00ee56db5
ELFrancisco4 <offiongfrancis14@gmail.com>
2025-02-21 17:44:47 +0100

    Increase search bar padding vertical value to 3

--- test/components/Search Bar/GalaxySearchBar.tsx
+++ test/components/Search Bar/GalaxySearchBar.tsx
@@ -5,7 +5,7 @@ import EvilIcons from '@expo/vector-icons/EvilIcons';
 const SearchBar = () => {
   return (
     <View className="mx-4 my-3">
-      <View className="flex-row items-center bg-gray-100 rounded-full px-4 py-2">
+      <View className="flex-row items-center bg-gray-100 rounded-full px-4 py-3">
         <EvilIcons name="search" size={24} color="gray" />
         <TextInput 
           placeholder='Search the galaxy...' 
