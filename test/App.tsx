66cba817e9ac2e0c7a6d3f4b3aa5585ea687da0c
khaymanii <kennethakpo61@gmail.com>
2025-02-22 23:15:57 +0100

    Remove SearchBar component from App and clean up ScrollView

--- test/App.tsx
+++ test/App.tsx
@@ -1,12 +1,9 @@
 import React from "react";
 import "./global.css";
 import { StyleSheet, ScrollView } from "react-native";
-import SearchBar from "./components/Search Bar/GalaxySearchBar";
 export default function App() {
   return (
-    <ScrollView style={styles.container} className="font-sans">
-      <SearchBar />
-    </ScrollView>
+    <ScrollView style={styles.container} className="font-sans"></ScrollView>
   );
 }
 
