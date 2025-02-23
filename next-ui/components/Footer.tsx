b56acd1d74b7e45ed56ebc249dcff2baa4993d45
khaymanii <kennethakpo61@gmail.com>
2025-02-23 09:42:49 +0100

    Update star count in Footer and Header components

--- next-ui/components/Footer.tsx
+++ next-ui/components/Footer.tsx
@@ -32,7 +32,7 @@ function Footer() {
             <Link href="https://github.com/khaymanii/alienui">
               <FaGithub size={24} />
             </Link>
-            <span className="font-medium text-md text-white">67⭐</span>
+            <span className="font-medium text-md text-white">73⭐</span>
           </div>
         </div>
         <div className="my-6 border-t border-brown-700"></div>
