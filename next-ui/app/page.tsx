c20eaf7b7edbe9fc9e13fe301a50794b9c0eb368
khaymanii <kennethakpo61@gmail.com>
2025-02-23 09:40:31 +0100

    fixed conflict

--- next-ui/app/page.tsx
+++ next-ui/app/page.tsx
@@ -4,14 +4,7 @@ import NewsLetter from "@/components/NewsLetter";
 import FaQ from "@/components/FaQ";
 import Inspiration from "@/components/Inspiration";
 import Contributors from "@/components/Contributors";
-<<<<<<< HEAD
-<<<<<<< HEAD
 import Products from "../components/Products";
-=======
-import GalaxySearchBar from "@/components/comp/Search Bar/GalaxySearchBar";
->>>>>>> 8a7873e9c75c03d245304860e1d5c20b4c771a49
-=======
->>>>>>> b33df513b3a08dbd9b441d5d455f5b87c12b644f
 
 export default function Home() {
   return (
