05933d51a616fe5a7333012501baff8b21e93736
Ken <116851212+khaymanii@users.noreply.github.com>
2025-02-25 22:49:16 +0100

    Update Header.tsx

--- next-ui/components/Header.tsx
+++ next-ui/components/Header.tsx
@@ -27,7 +27,7 @@ function Header() {
             <Link href="https://github.com/khaymanii/alienui">
               <FaGithub size={24} />
             </Link>
-            <span className="font-medium text-md">101⭐</span>
+            <span className="font-medium text-md">102⭐</span>
           </div>
 
           {/* Hamburger Menu (optional, if you want to toggle mobile menu later) */}
