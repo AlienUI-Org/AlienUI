31bcea52aa20647f71d9f4a69004e869fd50fbb0
ELFrancisco4 <offiongfrancis14@gmail.com>
2025-02-26 10:49:34 +0100

    Update padding and alignment of icons in jupitertab

--- test/components/Tab/JupiterTab.tsx
+++ test/components/Tab/JupiterTab.tsx
@@ -1,5 +1,5 @@
-import { View, Text, TouchableOpacity } from 'react-native'
-import React from 'react'
+import { View, Text, TouchableOpacity } from "react-native";
+import React from "react";
 import { AntDesign } from "@expo/vector-icons";
 
 const Tab = () => {
@@ -16,36 +16,36 @@ const Tab = () => {
       <View className="flex-row w-full items-center justify-around bg-zinc-900 rounded-t-3xl py-4 px-2 mt-8 relative">
         {/* Curved valley effect */}
         <View className="absolute -top-6 left-1/2 -translate-x-1/2 w-24 h-16 bg-white rounded-b-full mt-6" />
-        
-        <TouchableOpacity className="items-center">
+
+        <TouchableOpacity className="flex items-center">
           <View className="bg-transparent p-3">
             <AntDesign name="home" size={24} color="white" />
+            <Text className="text-gray-50 text-xs mt-0.5 text-center">Home</Text>
           </View>
-          <Text className="text-gray-50 text-xs mt-1">Home</Text>
         </TouchableOpacity>
 
-        <TouchableOpacity className="items-center">
-          <View className="bg-transparent p-3">
+        <TouchableOpacity className="flex items-center">
+          <View className="bg-transparent p-3 flex-col justify-center items-center">
             <AntDesign name="contacts" size={24} color="gray" />
+            <Text className="text-gray-400 text-xs mt-0.5 text-center">Contacts</Text>
           </View>
-          <Text className="text-gray-400 text-xs mt-1">Contacts</Text>
         </TouchableOpacity>
 
         {/* Empty space for center button */}
         <View className="w-16" />
 
-        <TouchableOpacity className="items-center">
-          <View className="bg-transparent p-3">
+        <TouchableOpacity className="flex items-center mr-4">
+          <View className="bg-transparent p-3 flex-col justify-center">
             <AntDesign name="shoppingcart" size={24} color="gray" />
+            <Text className="text-gray-400 text-xs mt-0.5 text-center">Cart</Text>
           </View>
-          <Text className="text-gray-400 text-xs mt-1">Cart</Text>
         </TouchableOpacity>
 
-        <TouchableOpacity className="items-center">
+        <TouchableOpacity className="flex items-center">
           <View className="bg-transparent p-3">
             <AntDesign name="barschart" size={24} color="gray" />
+            <Text className="text-gray-400 text-xs mt-0.5 text-center">Chart</Text>
           </View>
-          <Text className="text-gray-400 text-xs mt-1">Chart</Text>
         </TouchableOpacity>
       </View>
     </View>
