811b8d62d33be73a1d13708a15969a81f4eff108
ELFrancisco4 <offiongfrancis14@gmail.com>
2025-02-26 09:51:39 +0100

    Create Jupiter tab component

---
+++ test/components/Tab/JupiterTab.tsx
@@ -0,0 +1,55 @@
+import { View, Text, TouchableOpacity } from 'react-native'
+import React from 'react'
+import { AntDesign } from "@expo/vector-icons";
+
+const Tab = () => {
+  return (
+    <View className="flex items-center relative">
+      {/* Floating Action Button */}
+      <View className="absolute -top-10 z-10">
+        <View className="bg-black rounded-full p-4 shadow-lg mr-4 mt-14">
+          <AntDesign name="appstore1" size={24} color="white" />
+        </View>
+      </View>
+
+      {/* Navigation Bar */}
+      <View className="flex-row w-full items-center justify-around bg-zinc-900 rounded-t-3xl py-4 px-2 mt-8 relative">
+        {/* Curved valley effect */}
+        <View className="absolute -top-6 left-1/2 -translate-x-1/2 w-24 h-16 bg-white rounded-b-full mt-6" />
+        
+        <TouchableOpacity className="items-center">
+          <View className="bg-transparent p-3">
+            <AntDesign name="home" size={24} color="white" />
+          </View>
+          <Text className="text-gray-50 text-xs mt-1">Home</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity className="items-center">
+          <View className="bg-transparent p-3">
+            <AntDesign name="contacts" size={24} color="gray" />
+          </View>
+          <Text className="text-gray-400 text-xs mt-1">Contacts</Text>
+        </TouchableOpacity>
+
+        {/* Empty space for center button */}
+        <View className="w-16" />
+
+        <TouchableOpacity className="items-center">
+          <View className="bg-transparent p-3">
+            <AntDesign name="shoppingcart" size={24} color="gray" />
+          </View>
+          <Text className="text-gray-400 text-xs mt-1">Cart</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity className="items-center">
+          <View className="bg-transparent p-3">
+            <AntDesign name="barschart" size={24} color="gray" />
+          </View>
+          <Text className="text-gray-400 text-xs mt-1">Chart</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+export default Tab;
