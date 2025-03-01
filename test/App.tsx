f238a1de0f1682f69ebeadadef3ef02b3b9b8eb1
khaymanii <kennethakpo61@gmail.com>
2025-03-01 13:02:44 +0100

    Remove unused components and clean up App.tsx for improved readability

--- test/App.tsx
+++ test/App.tsx
@@ -1,18 +1,10 @@
 import React from "react";
 import "./global.css";
-import { StyleSheet, View, ScrollView  } from "react-native";
-import Avatar from "./components/Avatar/EarthAvatar";
-import JupiterTab from './components/Tab/JupiterTab';
-
+import { StyleSheet, ScrollView } from "react-native";
 
 export default function App() {
   return (
-    <ScrollView style={styles.container} className="font-sans">
-      <Avatar />
-      {/* Other components can be added here */}
-      <View style={styles.flexGrow} />
-      <JupiterTab />
-    </ScrollView>
+    <ScrollView style={styles.container} className="font-sans"></ScrollView>
   );
 }
 
@@ -20,8 +12,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-  },
-  flexGrow: {
-    flex: 1,
   },
 });
