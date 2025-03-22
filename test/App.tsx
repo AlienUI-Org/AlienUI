3ffc138604f35111f4168419db69a4416e27a8f2
“ShedrachJonah11” <ShedrachJonah11@gmail.com>
2025-03-22 03:54:49 +0100

    feat: add earth searchbar UI #15

--- test/App.tsx
+++ test/App.tsx
@@ -1,16 +1,10 @@
 import React from "react";
 import "./global.css";
-import { StyleSheet, ScrollView, Text, View } from "react-native";
-import GalaxyModal from "./components/Modal/GalaxyModal";
-import SearchBar from "./components/Search Bar/GalaxySearchBar";
+import { StyleSheet, ScrollView } from "react-native";
 
 export default function App() {
   return (
-    <ScrollView style={styles.container} className="font-sans">
-      <View style={{ marginTop: 200 }}>
-        <SearchBar />
-      </View>
-    </ScrollView>
+    <ScrollView style={styles.container} className="font-sans"></ScrollView>
   );
 }
 
--- test/components/Search Bar/EarthSearchBar.tsx
+++ test/components/Search Bar/EarthSearchBar.tsx
@@ -1,10 +1,18 @@
-import { View, Text } from 'react-native'
+import { View, TextInput } from 'react-native'
 import React from 'react'
+import EvilIcons from '@expo/vector-icons/EvilIcons'
 
 const EarthSearchBar = () => {
   return (
-    <View>
-      <Text>EarthSearchBar</Text>
+    <View className="mx-4 my-3">
+      <View className="flex-row items-center bg-gray-100 rounded-full px-4 py-3">
+        <EvilIcons name="search" size={24} color="gray" />
+        <TextInput
+          placeholder="Search the Earth..."
+          className="flex-1 ml-2 outline-none"
+          placeholderTextColor="gray"
+        />
+      </View>
     </View>
   )
 }
