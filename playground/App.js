5f4ae5f72704dce2dcedf652993fce691cffbc08
khaymanii <kennethakpo61@gmail.com>
2025-04-18 13:34:34 +0100

    finished second task

--- playground/App.js
+++ playground/App.js
@@ -1,15 +1,12 @@
 import { StyleSheet, ScrollView } from "react-native";
 import "./global.css";
-import TempScreen3 from "./screens/TempScreen3";
 
 export default function App() {
   return (
     <ScrollView
       style={styles.container}
       className="font-sans bg-black h-screen"
-    >
-      <TempScreen3 />
-    </ScrollView>
+    ></ScrollView>
   );
 }
 
