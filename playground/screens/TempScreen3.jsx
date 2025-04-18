e1648345cf3b5a03e886d4fc7f83c70dc8619e79
khaymanii <kennethakpo61@gmail.com>
2025-04-18 11:11:53 +0100

    imported assets

--- playground/screens/TempScreen3.jsx
+++ playground/screens/TempScreen3.jsx
@@ -1,11 +1,10 @@
 import { View } from "react-native";
-import React from "react";
 import Button from "../components/comp3/Button";
 
 const TempScreen3 = () => {
   return (
     <View>
-      <Button label="Get Started" />{" "}
+      <Button label="Get Started" />
     </View>
   );
 };
