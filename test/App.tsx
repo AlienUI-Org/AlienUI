dd67a751a2325b143f6585300f06a0cb7460ab62
khaymanii <kennethakpo61@gmail.com>
2025-05-03 16:17:46 +0100

    feat: update package.json and tailwind.config.js for improved dark mode support and new UI components
    - Added DARK_MODE environment variable to start scripts for Android, iOS, and web.
    - Updated dependencies including @gluestack-ui packages and react-native-safe-area-context.
    - Enhanced tailwind.config.js with dark mode support, safelist patterns, and extended theme colors.
    - Introduced new font families, font weights, and box shadows in tailwind configuration.
    - Updated tsconfig.json to include path mappings for easier imports.

--- test/App.tsx
+++ test/App.tsx
@@ -1,10 +1,17 @@
 import React from "react";
+import "@/global.css";
+import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 import "./global.css";
 import { StyleSheet, ScrollView } from "react-native";
+import Spinner from "./components/Spinner";
 
 export default function App() {
   return (
-    <ScrollView style={styles.container} className="font-sans"></ScrollView>
+    <GluestackUIProvider mode="light">
+      <ScrollView style={styles.container} className="font-sans">
+        <Spinner />
+      </ScrollView>
+    </GluestackUIProvider>
   );
 }
 
